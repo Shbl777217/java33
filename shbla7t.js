00f9930c937a10c33547302b756252fee6d8f42f
Shbl777217 <115428778+Shbl777217@users.noreply.github.com>
2023-01-14 04:11:28 +0300

    Update shbla7t.js

--- shbla7t.js
+++ shbla7t.js
@@ -1591,4 +1591,135 @@ background-color:#000;}
 
 
 
+  $("style").last().append(`
+
+
+
+#users .uzr.khil span.uhash{
+    text-transform:uppercase;
+    -webkit-text-fill-color:#000;
+    display:inline-block;}
+
+
+#users .uzr.khil img.u-ico {
+    
+     margin-left: 9px;
+      
+    }
+
+#users .uzr.khil img.ustat {
+      width: 0px !important;
+      border-radius: 100px;
+      height: 0px;
+     
+      
+    }
+
+#users .uzr.khil .fitimg.u-pic {
+  border-radius: 100px !important;
+  margin-top: 6px !important;
+  height: 52px !important;
+  margin-left: 12px!important;
+  z-index: 0 !important;
+margin-bottom: 9px !important;
+padding: 27px !important;
+}
+
+#users .uzr.khil .u-msg {
+	text-align: center;
+  -webkit-background-clip: text;
   
+  font-size: 93% !important;
+  -webkit-text-fill-color: #0000;
+ background-image: url(https://up6.cc/2023/01/167338950726541.gif);
+  padding: 0px !important;
+	margin-bottom: 20px !important;
+margin-left: 10px;
+background-size: cover;
+margin-top: 8px !important;
+
+}
+
+
+
+
+
+#users .uzr.khil {
+  box-shadow: inset 0 0 0 rgba(0,0,0,.08),0 0 2px #000;
+  margin-bottom: 3px !important;
+  margin-top: 2px !important;
+  border-radius: 4px;
+  background-image: 
+  border: 1px solid #fff;
+background-image: url(https://up6.cc/2023/01/167364594709351.gif);
+background-size: 100%;
+}
+
+
+
+#users .uzr.khil .d-flex.fl {
+  padding-right: 0px !important;
+}
+
+#users .uzr.khil .u-topic {
+-webkit-text-fill-color: transparent;
+  margin-left: 5px;
+}
+
+#users .uzr.khil .My_star18 {
+ width: 75px;
+height: 71px;
+margin-top: -35px;
+cursor: pointer;
+margin-left: -37px;
+}
+
+
+#users .uzr.khil .My_vedio7 {
+  float: right;
+  width: 20px;
+  margin-top: 1px;
+  margin-right: 0px;
+  cursor: pointer;
+}
+
+
+`);
+
+var l_itvkhil=setInterval(function(){
+    if(myid!=null){
+        clearInterval(l_itvkhil);
+       $("#users .uzr:contains('*كحيلان')").addClass('khil');
+		
+		$("#users .uzr span.u-topic:contains('*كحيلان')").after('<img id="mystory" class="My_vedio7" title="ستوري" src="https://i.ibb.co/47VS4nY/image.png">');
+		
+		
+        $("#users .uzr.khil .fitimg.u-pic").append('<img class="My_star18" title="اطاري" src="https://up6.cc/2023/01/16736447415681.gif">');
+		
+		
+		$("#users .uzr.khil span.uhash, #users .uzr.khil img.co").hide(); 
+
+
+        $("#upro").before('<div id="movon" onclick="$(this).hide().html(``);" class="moveon" style="display:none;width:100%;height:100%;z-index:999999;position: fixed;left: 0px;top: 0px;background-color: rgba(0, 0, 0, 0.6);">'); 
+        
+        
+        
+        $("div#users .uzr:contains('*كحيلان') .My_vedio7").click(function(){
+        $("#movon").show();
+        
+        $('#movon').addClass('khil');
+        $("#movon").before('<div class="modal-dialog fr break" style="height: 338px; max-width: 340px; min-width: 300px; min-height: 599px;overflow: visible;margin-top:40px;"></div>');
+        $("#movon").append('<div style="max-width: 340px; min-width: 300px; color:white; width: 338px; float: right;" class="modal-header label-primary"><span class="pull-right clickable badge"><i class="fa fa-times"></i></span><label style="margin:1px; float: right;" > My vedio </label></div>');
+        $("#movon").append('<iframe id="ifr_mov7" style="max-width: 340px;min-width: 300px;right: 0; top: 35px; left: auto;" class="modal in" role="dialog" style="z-index: 2100; display: none; style="max-width:240px;" src="https://www.youtube.com/embed/L8V8AASAmMU" allowfullscreen="" width="338" height="490" frameborder="0"></iframe>');
+       $("#ifr_mov7").show(function(){
+        $("#upro").modal("hide");
+    } ); 
+});
+    }
+}, 20000);
+
+
+
+     
+		
+
